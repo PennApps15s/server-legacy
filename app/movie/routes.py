474521bd0a2a77a23d68293e9f22a7273b88ce59
Christopher Su <christophersu9@gmail.com>
2015-01-17 21:06:42 -0500

    resolve conflict

--- app/movie/routes.py
+++ app/movie/routes.py
@@ -9,6 +9,21 @@ from datetime import datetime
 
 movies_blueprint = Blueprint('movies', __name__, url_prefix='/movie')
 
+@movies_blueprint.route('/feed', methods=['GET'])
+@requires_login
+def get_movie_feed():
+    result = db.engine.execute('''SELECT movies.id
+        FROM movies 
+        WHERE movies.id 
+        NOT IN (
+            SELECT reviews."movieId" 
+            FROM reviews 
+            WHERE reviews."userId" = ''' + g.user.id + ')')
+
+    print result
+
+    return str(user), 200, {'Content-Type': 'application/json'}
+
 @movies_blueprint.route('/<movie_id>/', methods=["GET"])
 @requires_login
 def get_movie(movie_id):
